6937386775ba13537c32dd85ab6d72da255dcb54
Rice <ricen@CSGP.EDU>
2025-08-27 14:43:51 -0600

    finished

---
+++ burn.py
@@ -0,0 +1,9 @@
+name = input("What is your name?:")
+age = input("How old are you?:")
+color = input("What is your favorite color?:")
+hobby = input("What is your favorite hobby?:")
+joke = input("Tell me a joke:")
+
+
+print(f"Wow, who came up with the name {name}, I definitely would NOT be talking to them anymore.Your {age}? You definitely don't act like it. Did you say your favorite color is {color}? What are you 5? Don't get me started on your 'hobby', {hobby}, that might just be THE lamest thing i've ever witnessed. Oh, and your ‘joke’: {joke}, might be the unfunniest thing I've ever heard, I don't hear ANYONE laughing.")
+
